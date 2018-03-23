af2a9e4e73d788bd10f0c0f25c0b34b807c6d636
Ryan Miles <ryanm1114@gmail.com>
2018-03-23 19:40:42 -0400

    Base implementation of cache and realm Also refactored DailyEntry -> Entry and Entry -> HourlyEntry

--- app/src/main/java/me/ryanmiles/dailyenergytracker/data/cache/EntryRepository.kt
+++ app/src/main/java/me/ryanmiles/dailyenergytracker/data/cache/EntryRepository.kt
@@ -2,22 +2,68 @@ package me.ryanmiles.dailyenergytracker.data.cache
 
 import android.util.Log
 import me.ryanmiles.dailyenergytracker.data.interfaces.EntryDataSource
-import me.ryanmiles.dailyenergytracker.data.model.DailyEntry
+import me.ryanmiles.dailyenergytracker.data.model.Entry
 import me.ryanmiles.dailyenergytracker.data.source.RealmDataSource
 import java.util.*
 
 /*
  * Created by Ryan Miles on 3/20/2018.
  */
-class EntryRepository(val realmDataSource: RealmDataSource) : EntryDataSource {
-    override fun getEntry(entryId: String, callback: EntryDataSource.getEntryCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class EntryRepository(val realmDataSource: EntryDataSource) : EntryDataSource {
+
+    override fun saveEntry(entry: Entry): Entry {
+        val realmEntry = realmDataSource.saveEntry(entry)
+        cache(realmEntry)
+        return realmEntry
     }
+
+    override fun deleteAllEntries() {
+        realmDataSource.deleteAllEntries()
+        cachedEntries.clear()
+    }
+
+    override fun deleteEntry(entryId: String) {
+        realmDataSource.deleteEntry(entryId)
+        cachedEntries.remove(entryId)
+    }
+
+    /**
+     * Gets tasks from local data source (realm)
+     *
+     * Note: [EntryDataSource.GetEntryCallback.onDataNotAvailable] is fired if data source fails to
+     * get the data.
+     */
+    override fun getEntry(entryId: String, callback: EntryDataSource.GetEntryCallback) {
+        val entryInCache = getEntryWithId(entryId)
+
+        // Respond immediately with cache if available
+        if (entryInCache != null) {
+            callback.onEntryLoaded(entryInCache)
+            return
+        }
+
+
+        // Load the entry from the local data source
+        realmDataSource.getEntry(entryId, object : EntryDataSource.GetEntryCallback {
+            override fun onEntryLoaded(entry: Entry) {
+                // Do in memory cache update to keep the app UI up to date
+                cache(entry)
+                callback.onEntryLoaded(entry)
+
+            }
+
+            override fun onDataNotAvailable() {
+                callback.onDataNotAvailable()
+            }
+        })
+    }
+
+    private fun getEntryWithId(entryId: String) = cachedEntries[entryId]
 
     /**
      * This variable has public visibility so it can be accessed from tests.
      */
-    var cachedEntries: LinkedHashMap<String, DailyEntry> = LinkedHashMap()
+    var cachedEntries: LinkedHashMap<String, Entry> = LinkedHashMap()
 
     /**
      * Marks the cache as invalid, to force an update the next time data is requested. This variable
@@ -25,7 +71,7 @@ class EntryRepository(val realmDataSource: RealmDataSource) : EntryDataSource {
      */
     var cacheIsDirty = false
 
-    fun refreshEntries() {
+    override fun refreshEntries() {
         cacheIsDirty = true
     }
 
@@ -45,7 +91,7 @@ class EntryRepository(val realmDataSource: RealmDataSource) : EntryDataSource {
         } else {
             // Query the Realm
             realmDataSource.getEntries(object : EntryDataSource.LoadEntriesCallback {
-                override fun onEntriesLoaded(entries: List<DailyEntry>) {
+                override fun onEntriesLoaded(entries: List<Entry>) {
                     refreshCache(entries)
                     callback.onEntriesLoaded(ArrayList(cachedEntries.values))
                 }
@@ -58,14 +104,18 @@ class EntryRepository(val realmDataSource: RealmDataSource) : EntryDataSource {
 
     }
 
-    private fun refreshCache(entries: List<DailyEntry>) {
+    private fun refreshCache(entries: List<Entry>) {
         cachedEntries.clear()
         entries.forEach {
-            val cachedEntry = DailyEntry(it.id, it.date, it.note, it.entries)
-            cachedEntries[cachedEntry.id] = cachedEntry
+            cache(it)
         }
 
         cacheIsDirty = false
+    }
+
+    private fun cache(entry: Entry) {
+        val cachedEntry = Entry(entry.id, entry.date, entry.note, entry.hourlyEntries)
+        cachedEntries[cachedEntry.id] = cachedEntry
     }
 
     companion object {
