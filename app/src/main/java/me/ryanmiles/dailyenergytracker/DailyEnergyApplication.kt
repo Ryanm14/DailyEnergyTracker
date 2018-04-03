967b472e8d4ae24a0d76218d5a8498301cffc54f
Ryan Miles <ryanm1114@gmail.com>
2018-04-03 13:38:16 -0400

    Remove Stetho

--- app/src/main/java/me/ryanmiles/dailyenergytracker/DailyEnergyApplication.kt
+++ app/src/main/java/me/ryanmiles/dailyenergytracker/DailyEnergyApplication.kt
@@ -1,8 +1,6 @@
 package me.ryanmiles.dailyenergytracker
 
 import android.app.Application
-import com.facebook.stetho.Stetho
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider
 import io.realm.Realm
 
 
@@ -19,10 +17,5 @@ class DailyEnergyApplication : Application() {
         super.onCreate()
         Realm.init(this)
 
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
-                        .build())
     }
 }
